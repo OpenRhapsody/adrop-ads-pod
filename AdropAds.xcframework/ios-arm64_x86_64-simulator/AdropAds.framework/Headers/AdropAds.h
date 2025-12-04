1b0a4c800e5c3984f832bb65794fd34c76b2f732
Leo <hc@adrop.io>
2025-12-04 14:17:56 +0900

    1.7.1-alpha.0

---
+++ AdropAds.xcframework/ios-arm64_x86_64-simulator/AdropAds.framework/Headers/AdropAds.h
@@ -0,0 +1,14 @@
+//
+//  AdropAds.h
+//  AdropAds
+//
+//  Created by Leo on 3/12/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AdropAds.
+FOUNDATION_EXPORT double AdropAdsVersionNumber;
+
+//! Project version string for AdropAds.
+FOUNDATION_EXPORT const unsigned char AdropAdsVersionString[];
